2590e180fc45de8da7c8e0c944d66fc6a1dbd314
mutatio <mac@macdeMacBook-Pro.local>
2025-04-15 23:56:08 +0800

    learn swift ui

--- LearnSwiftUI/LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/LearnSwiftUI/ContentView.swift
@@ -10,14 +10,81 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    
+    @State private var choice = 0
+
+    func applyChanges() {
+        
+    }
+    
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
             Text("Hello, world!")
+                .font(.largeTitle)
+            Text("Text")
+                .font(.caption)
+                .italic()
         }
         .padding()
+        
+        HStack {
+            Image(systemName: "folder.badge.plus")
+            Image(systemName: "heart.circle.fill")
+            Image(systemName: "alarm")
+        }
+        .symbolRenderingMode(.multicolor)
+        .font(.largeTitle)
+        
+        HStack {
+            Label("Favorite Books", systemImage: "books.vertical")
+                .labelStyle(.titleAndIcon)
+                .font(.largeTitle)
+        }
+        .padding()
+        
+        VStack {
+            HStack {
+                Picker("Choice", selection: $choice) {
+                    Text("Option 1").tag(0)
+                    Text("Option 2").tag(1)
+                    Text("Option 3").tag(2)
+                }
+                Button("OK") {
+                    applyChanges()
+                }
+            }
+            .controlSize(.mini)
+            HStack {
+                Picker("Choice", selection: $choice) {
+                    Text("Option 1").tag(0)
+                    Text("Option 2").tag(1)
+                    Text("Option 3").tag(2)
+                }
+                Button("OK") {
+                    applyChanges()
+                }
+            }
+            .controlSize(.large)
+        }
+
+        HStack {
+            Image("TUTU")
+                .resizable()
+                .scaledToFit()
+        }
+        
+        HStack {
+            Rectangle()
+                .foregroundColor(.blue)
+            Circle()
+                .foregroundColor(.orange)
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .foregroundColor(.green)
+        }
+        .aspectRatio(3.0, contentMode: .fit)
+        
+        
+        
     }
 }
 
